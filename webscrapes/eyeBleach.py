2f4bb9baac0bc2f521a6f42ada04492359c0f226
Daegybyte <daegybyte@protonmail.ch>
2021-05-07 17:27:18 -0600

    changed search youtube to use send_keys method instead of the click method

--- webscrapes/eyeBleach.py
+++ webscrapes/eyeBleach.py
@@ -5,39 +5,8 @@ import random
 search = ["corgis", "corgi play", "corgi pirates", "corgi loaf", "corgi butt", "corgi sleeping", "puppies", "cute dogs", "cute cat", "kittens", "manatees"]
 website = ["youtube","google"]
 
-# rand = int(random.randint(0,len(search)-1))
-
-# driver = webdriver.Chrome()
-
-# # search Google images
-# driver.get('https://www.google.com/imghp?hl=en&ogbl') # goes to the google images page
-
-# gsearchbox = driver.find_element_by_xpath('//*[@id="sbtc"]/div/div[2]/input') # navigates to searchbox
-
-# gsearchbox.send_keys(search[rand]) # enters element from search array
-
-# gsearchbox.send_keys(Keys.RETURN) #enter key to start image search
-
-
-
-# search Youtube
-# driver.get('https://youtube.com')
-
-# ytsearchbox = driver.find_element_by_xpath('//*[@id="search"]')
-# ytsearchbox.send_keys(search[rand])
-
-# ytsearchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
-# ytsearchButton.click()
-
-
-
-
-
-
-
-
 pickSearch = int(random.randint(0,len(search)-1))
-pickWeb = int(random.randit(0,2))
+pickWeb = int(random.randint(0,len(website)-1))
 
 driver = webdriver.Chrome()
 
@@ -58,4 +27,4 @@ else:
     ytsearchbox.send_keys(search[pickSearch])
 
     ytsearchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
-    ytsearchButton.click()
+    ytsearchButton.send_keys(Keys.RETURN)
