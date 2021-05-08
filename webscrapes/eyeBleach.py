40d48c41f2e603be593a90d7873de3a69a0eb216
Daegybyte <daegybyte@protonmail.ch>
2021-05-08 09:54:55 -0600

    added new search terms and reordered website vare to match if/else order

--- webscrapes/eyeBleach.py
+++ webscrapes/eyeBleach.py
@@ -1,30 +1,36 @@
+# $ pip3 install selenium
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys 
 import random
 
-search = ["corgis", "corgi play", "corgi pirates", "corgi loaf", "corgi butt", "corgi sleeping", "puppies", "cute dogs", "cute cat", "kittens", "manatees"]
-website = ["youtube","google"]
+search = [
+    "corgis", "corgi play", "corgi pirates", "corgi loaf", "corgi butt", "corgi sleeping",
+    "puppies", "cute dogs", "cute cat", "kittens", "manatees", "baby animals", "baby goats",
+    "baby sloths", "baby turtles", "snakes in hats"
+        ]
+
+website = ["google", "youtube"]
 
 pickSearch = int(random.randint(0,len(search)-1))
 pickWeb = int(random.randint(0,len(website)-1))
 
 driver = webdriver.Chrome()
 
-if (pickWeb == 1 ):
+if (pickWeb == 0 ):
     # search Google images
     driver.get('https://www.google.com/imghp?hl=en&ogbl') # goes to the google images page
 
-    gsearchbox = driver.find_element_by_xpath('//*[@id="sbtc"]/div/div[2]/input') # navigates to searchbox
+    gSearchbox = driver.find_element_by_xpath('//*[@id="sbtc"]/div/div[2]/input') # navigates to searchbox
 
-    gsearchbox.send_keys(search[pickSearch]) # enters element from search array
+    gSearchbox.send_keys(search[pickSearch]) # enters element from search array
 
-    gsearchbox.send_keys(Keys.RETURN) #enter key to start image search
+    gSearchbox.send_keys(Keys.RETURN) #enter key to start image search
 else:
     #search Youtube
     driver.get('https://youtube.com')
 
-    ytsearchbox = driver.find_element_by_xpath('//*[@id="search"]')
-    ytsearchbox.send_keys(search[pickSearch])
+    ytSearchbox = driver.find_element_by_xpath('//*[@id="search"]')
+    ytSearchbox.send_keys(search[pickSearch])
 
-    ytsearchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
-    ytsearchButton.send_keys(Keys.RETURN)
+    ytSearchbox.send_keys(Keys.RETURN)
